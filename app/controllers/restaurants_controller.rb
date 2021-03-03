fc4fad7d2c336bda8f34b3a111d88078b85948be
Mehdi Seffar <mehdi.seffar@outlook.fr>
2021-03-03 15:26:48 +0100

    exercice 2 done

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -2,4 +2,13 @@ class RestaurantsController < ApplicationController
   def index
     @restaurants = Restaurant.all
   end
+
+  def show
+    @restaurant = Restaurant.find(params[:id])
+    @review = Review.new
+  end
+
+  def new
+    @restaurant = Restaurant.new # needed to instantiate the form_for
+  end
 end
--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,2 +1,26 @@
 class ReviewsController < ApplicationController
+  before_action :set_restaurant, only: [:new, :create]
+
+  def create
+    @review = Review.new(review_params)
+
+    # we need `restaurant_id` to associate review with corresponding restaurant
+    # @restaurant = Restaurant.find(params[:restaurant_id])
+    @restaurant = @review.restaurant
+    if @review.save
+      redirect_to restaurant_path(@restaurant)
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def review_params
+    params.require(:review).permit(:content, :rating)
+  end
+
+  def set_restaurant
+    @restaurant = Restaurant.find(params[:restaurant_id])
+  end
 end
